dedd64ec401b7cf49f71506783223c35bf87c9e8
forafish <fora.luo@gmail.com>
2018-05-31 18:09:49 +0800

    add game doc-types

---
+++ src/constants/doc-types.js
@@ -0,0 +1,14 @@
+export default {
+  game: {
+    "type":"Game",
+    "packages": "playing-game-elements",
+    "facets":[
+      "Versionable",
+      "Publishable",
+      "Commentable",
+      "HasRelatedText",
+      "Tagable",
+      "Downloadable"
+    ]
+  }
+};
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import DocTypes from './constants/doc-types';
+import GameEntity from './entities/game.entity';
+
+export default {
+  DocTypes,
+  GameEntity
+};
